81917b3782b2b2c215a5aa18419db756daa054bb
Gautam Gupta <ggautamg23@gmail.com>
2025-09-02 23:55:16 +0530

    Variables

--- JavaScripts/first.js
+++ JavaScripts/first.js
@@ -1,2 +1,19 @@
 console.log("Gautam Gupta");
 console.log("Learning JavaScript");
+
+//Variables
+fullName = "Tony Stark";
+age=24;
+price=99.99;
+x = null; // no value of null
+y = undefined;
+
+
+//undefined ka meaning PTA HI NHI ISKE ANDR KYA H
+//null ka meaning PTA TOH H ISKE ANDR KYA H PAR ISKE ANDR KHALI VALUE H
+
+
+//If we have to print my variables value direct
+//Do not use "fullName" this
+//Use direct fullName
+console.log(y);
