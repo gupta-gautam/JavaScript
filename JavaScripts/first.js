00385e821752f98ea56fd3a1289a5ca627a5f39a
Gautam Gupta <ggautamg23@gmail.com>
2025-09-03 00:41:50 +0530

    Data Types

--- JavaScripts/first.js
+++ JavaScripts/first.js
@@ -13,9 +13,13 @@ y = undefined;
 
 
 //Null and undefined have little bit difference
-//undefined ka meaning PTA HI NHI ISKE ANDR KYA H
-//null ka meaning PTA TOH H ISKE ANDR KYA H PAR ISKE ANDR KHALI VALUE H
+//Undefined ka meaning PTA HI NHI ISKE ANDR KYA H for eg:-
+let a;
+console.log(a);
 
+
+//Null ka meaning PTA TOH H ISKE ANDR KYA H PAR ISKE ANDR KHALI VALUE H
+//Null is a type of Object
 
 //Boolean type value
 isFollow = false;
@@ -33,7 +37,53 @@ console.log(FULLNAME);
 Console ="apna";
 console.log(Console);
 
+
+//let,const,var
+
 //We will use let , const , var not to re-declared things
 let name = "Gautam";
 let age = 25;
 console.log(name);
+
+const PI =3.14;
+const e = 2.7;
+console.log(PI);
+
+//const a;
+//console.log(a);
+//will show error;
+
+{
+  let age = 20;
+ // let a =10; only one let of same variable in one block {}
+  console.log(age);
+}
+
+{
+ let age = 10;
+ console.log(age);
+}
+
+//Data Types
+
+let int =10;
+//write typeof int it will give number
+
+//BigInt it will show n in last for eg:- 123n
+let m = BigInt(123);
+
+//Symbol
+let z =Symbol("Hello!");
+
+//Objects is a collection of values
+//key : value for eg:-
+
+const student = {
+fullname : "Gautam",
+age : 18,
+cgpa : 9,
+isPass : true,
+};
+//To access key there are two types obj.key or obj["key"]
+console.log(student["age"]);
+console.log(student.cgpa);
