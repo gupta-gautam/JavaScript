09739507151b3da129cfec387bc163d92ae3c74f
Gautam Gupta <ggautamg23@gmail.com>
2025-09-03 00:02:07 +0530

    variables

--- JavaScripts/first.js
+++ JavaScripts/first.js
@@ -1,3 +1,5 @@
+// JavaScript is Dynamically Typed Language
+
 console.log("Gautam Gupta");
 console.log("Learning JavaScript");
 
@@ -5,15 +7,20 @@ console.log("Learning JavaScript");
 fullName = "Tony Stark";
 age=24;
 price=99.99;
+radius = 14;
 x = null; // no value of null
 y = undefined;
 
 
+//Null and undefined have little bit difference
 //undefined ka meaning PTA HI NHI ISKE ANDR KYA H
 //null ka meaning PTA TOH H ISKE ANDR KYA H PAR ISKE ANDR KHALI VALUE H
 
 
+//Boolean type value
+isFollow = false;
+
 //If we have to print my variables value direct
 //Do not use "fullName" this
 //Use direct fullName
-console.log(y);
+console.log(isFollow);
