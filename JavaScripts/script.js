05898a78d5d2250ad1494cf3b87bb8ecea57283e
Gautam Gupta <ggautamg23@gmail.com>
2025-09-04 13:21:46 +0530

    operators

--- JavaScripts/script.js
+++ JavaScripts/script.js
@@ -1,1 +1,19 @@
+// This code prints hello world
 console.log("hello world!");
+
+
+//Arithmetic Operators
+let a = 5;
+let b = 2;
+
+console.log("a = ",a , "& b =" , b);
+console.log("a + b = ", a + b );
+console.log("a - b = ", a - b );
+console.log("a * b = ", a * b );
+console.log("a / b = ", a / b );
+
+// Modulus % gives Remainder for eg :- 5 % 2 = 1
+console.log("a % b = ", a % b );
+
+// Exponentiation  for eg:- a ** b ,  2 ** 3 = 8
+console.log("a ** b = ", a ** b ); // 5^2 = 25
