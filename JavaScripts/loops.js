dc63e2edbc9ad9d6ec9c371ff45211bb2f66fa72
Gautam Gupta <ggautamg23@gmail.com>
2025-09-07 01:04:23 +0530

    loops

--- JavaScripts/loops.js
+++ JavaScripts/loops.js
@@ -29,3 +29,52 @@ do{
 console.log("j=",j);
 j++;
 } while(j<=8);
+
+
+// For-of Loops
+// is for Strings and Arrays Not for Objects
+let str = "Gautam";
+
+let size =0;
+for( let z of str){ // iterator -> characters
+console.log(z);
+size++;
+}
+
+console.log("string size =",size);
+
+
+// For-in Loops
+// is for Objects and Arrays
+let student = {
+name : "rahul",
+age : 18,
+cgpa : 9,
+isPass : true,
+};
+
+// will return key
+for(let key in student){
+console.log("key=",key ,"value=",student[key]);
+}
+
+//Print all even from 0 to 100
+for(let num=0;num<=100;num++){
+if(num % 2 === 0){
+console.log(num);
+}else{
+}
+}
+
+// Game question
+let gameNum = 25;
+
+let userNum = prompt("Guess the game number :");
+
+while(userNum != gameNum){ // game
+
+userNum = prompt("You entered wrong number. Guess again :");
+
+}
+
+console.log("congratulations you entered the right number");
