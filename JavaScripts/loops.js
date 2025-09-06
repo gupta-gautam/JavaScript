b1eee931bfab5aa898985ca4fea12d26e339a54f
Gautam Gupta <ggautamg23@gmail.com>
2025-09-07 00:24:34 +0530

    loops

---
+++ JavaScripts/loops.js
@@ -0,0 +1,31 @@
+// Loops
+
+// For Loops
+for(let i=1;i<=10;i++){
+console.log(i);
+}
+
+console.log("loop has ended");
+
+// Calculate sum of 1 to 5
+let sum = 0;
+for(let i = 1; i <= 5; i++ ){
+sum += i;
+}
+console.log(sum);
+
+
+// While Loops
+let i =1;
+while(i<=5){
+console.log("i=",i);
+i++;
+}
+
+// Do-While Loops
+
+let j = 1;
+do{
+console.log("j=",j);
+j++;
+} while(j<=8);
--- JavaScripts/strings.js
+++
@@ -1,7 +0,0 @@
-// Loops
-
-for(let i=1;i<=1000;i++){
-console.log("gautam");
-}
-
-console.log("loop has ended");
