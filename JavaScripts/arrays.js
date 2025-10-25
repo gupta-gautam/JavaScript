a528e8085d0a0d7e3ea67acf764f9bbeead29c3e
Gautam Gupta <ggautamg23@gmail.com>
2025-10-26 00:27:55 +0530

    JS

--- JavaScripts/arrays.js
+++ JavaScripts/arrays.js
@@ -4,5 +4,87 @@ let marks = [97,85,89,90];
 console.log(marks);
 console.log(marks.length); //property
 
-let cities = ["delhi","bangalore","mumbai","patna"];
-console.log(cities);
+let cities = ["delhi","bangalore","mumbai","patna","jhansi","mount abu"];
+
+// for loop
+for(let i =0;i<cities.length;i++){
+console.log(cities[i]);
+}
+
+// for of
+for(let city of cities){
+console.log(city.toUpperCase());
+}
+
+
+// Lets's Practice
+let m = [85,97,44,37,76,60];
+let sum =0;
+for(let val of m){
+sum+=val;
+}
+let avg = sum / m.length;
+console.log(`avg marks of class = ${avg}`);
+
+
+let items = [250,645,300,900,50];
+let i = 0;
+for(let val of items){
+let offer = val /10;
+items[i] = items[i] - offer;
+console.log(`value after offer = ${items[i]}`);
+i++;
+}
+
+// PUSH add to end
+let food = ["potatao","litchi","mango","papaya"];
+food.push("burger","chips","pizza")
+
+//POP removes from end
+console.log(food.pop());
+
+// ToString converts arrays to strings
+console.log(food.toString());
+
+//Concat join multiple arrays
+let male =["Rahul","Gautam","Mohan"];
+let female =["Priya","Riya"];
+
+let gender = male.concat(female);
+console.log(gender);
+
+//Unshift add to start
+female.unshift("ruhi");
+console.log(female);
+
+// Shift delete
+male.shift();
+console.log(male);
+
+//Slice returns a piece of array
+// in this original array not change
+console.log(male.slice(1,2));
+
+//Splice change original array(add,remove,replace)
+ let arr = [1,2,3,4,5,6,7,8];
+ //arr.splice(2,2,101,102);
+
+ // Add Element
+ arr.splice(2,0,101);
+
+ // Delete Element
+ arr.splice(4,1);
+
+ //Replace Element
+ arr.splice(3,1,101);
+
+// Lets's Practice
+let companies = ["bloomberg","microsoft","uber","google","IBM","netfix"];
+console.log(companies.shift());
+console.log(companies);
+
+console.log(companies.splice(1,1,"Ola"));
+console.log(companies);
+
+console.log(companies.push("Amazon"));
+console.log(companies);
---
+++ JavaScripts/functions.js
@@ -0,0 +1,120 @@
+function myFunction(){
+console.log("Welcome to this world");
+console.log("I am learning JS :)");
+}
+myFunction();
+
+function yourFunction(msg,n){  // parameter -> input
+console.log(msg,n);
+}
+yourFunction("JavaScript",100); // argument
+
+//Function-> 2 numbers , sum
+
+function sum(a,b){
+// local variables -> scope
+console.log(a+b);
+}
+sum(10,2);
+
+
+// Difference
+function diff(x,y){
+s = x - y;
+return s;
+}
+let val = diff(5,4);
+console.log(val);
+
+// Arrow Functions
+
+function addition(a,b){
+return a + b;
+}
+// Modern JS
+const arrowSum = (a,b)=>{
+console.log(a+b);
+};
+
+
+// Multiplication
+function mul(i,j){
+return i * j;
+}
+
+const arrowMul = (i,j) =>{
+return i * j;
+};
+
+const printHello = () =>{
+console.log("Hello");
+}
+
+// Let's Practice
+function countVowels(str){
+let count =0 ;
+for(const char of str){
+if( char === "a"|| char ==="e" || char ==="i"||char ==="o"|| char === "u"){
+count++;
+}
+}
+return count;
+}
+
+const countVow = (str) => {
+let count =0 ;
+for(const char of str){
+if( char === "a"|| char ==="e" || char ==="i"||char ==="o"|| char === "u"){
+count++;
+}
+}
+return count;
+}
+
+// For Each in Arrays
+
+let arr= [1,2,3,4,5];
+
+//arr.forEach(function printVal(val){ // value at each index
+//console.log(val);
+//})
+
+arr.forEach((val,idx,arr)=>{
+console.log(val,idx,arr);
+});
+
+// Lets's Practice
+
+let array = [1,2,3,4,5,];
+array.forEach((val)=>{
+console.log(val*val);
+})
+
+// MAP
+
+let  nums = [43,54,67];
+let newArray =nums.map((val)=>{
+return val;
+});
+console.log(newArray);
+
+
+// FIlTER
+let num = [1,2,3,4,5,6,];
+
+let even = arr.filter((val)=>{
+return val % 2 ===0;
+});
+
+console.log(even);
+
+//REDUCE
+
+let array1 = [1,5,3];
+
+const output = arr.reduce((prev,curr)=>{
+return  prev > curr ? prev :curr;
+});
+console.log(output);
+
+//Lets's Practice
