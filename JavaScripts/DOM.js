6e6b7f618466f8c47602f7427d345053659a65bb
Gautam Gupta <ggautamg23@gmail.com>
2025-10-26 21:07:05 +0530

    DOM

---
+++ JavaScripts/DOM.js
@@ -0,0 +1,23 @@
+console.log("hello");
+window.alert("hi");
+console.dir(window.document);
+console.log(document.body);
+console.dir(document.body);
+//console.log(document.body.childNodes[3].innerText = "abcd");
+
+// Id
+let heading = document.getElementById("heading"); // h1
+console.log(heading);
+console.dir(heading);
+
+// Class
+let headings = document.getElementsByClassName("head");
+console.dir(headings);
+console.log(headings);
+
+// Tag
+let para = document.getElementsByTagName("p");
+console.log(para);
+console.dir(para);
+
+
