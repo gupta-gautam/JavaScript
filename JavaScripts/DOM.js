7809d5f3b35ab229b336b880db577521a63e2b71
Gautam Gupta <ggautamg23@gmail.com>
2025-10-27 00:33:59 +0530

    Dom

--- JavaScripts/DOM.js
+++ JavaScripts/DOM.js
@@ -11,7 +11,7 @@ console.log(heading);
 console.dir(heading);
 
 // Class
-let headings = document.getElementsByClassName("head");
+let headings = document.getElementsByClassName("heads");
 console.dir(headings);
 console.log(headings);
 
@@ -19,5 +19,28 @@ console.log(headings);
 let para = document.getElementsByTagName("p");
 console.log(para);
 console.dir(para);
+
+// Query Selector
+let elements = document.querySelector(".heads"); // 1st element
+console.dir(elements);
+console.log(elements);
+
+let AllElements = document.querySelectorAll("p"); // All element
+console.dir(AllElements);
+console.log(AllElements);
+
+//Tag Name
+console.log(elements.tagName);
+
+// Inner Text
+let div = document.querySelector("div");
+console.dir(div);
+console.log(div.innerText);
+
+// Inner HTML
+console.log(div.innerHTML);
+
+let h = document.querySelector("h3");
+console.log(h);
 
 
