33103b03bb3462ec3f40b5f8633012a150061185
Gautam Gupta <ggautamg23@gmail.com>
2025-10-31 13:36:20 +0530

    ClassList

--- JavaScripts/DOM2.js
+++ JavaScripts/DOM2.js
@@ -20,11 +20,50 @@ console.log(para);
 let divs = document.querySelector("div");
 console.log(div);
 console.log(div.style);
-console.log(div.style.backgroundColor = "magenta"); // dynamically color change ho gya
+//console.log(div.style.backgroundColor = "magenta"); // dynamically color change ho gya
 console.log(div.style.fontSize="26px");
-console.log(div.style.width="200px")
-console.log(div.style.innerText = "HELLO!"); // ye daalne se site pr change ni hoga
-console.log(div.style.textAlign="center");
-div.innerText ="Hello!"; // but ye daalne se site pr change ho jayga
+//console.log(div.style.width="200px")
+//console.log(div.style.innerText = "HELLO!"); // ye daalne se site pr change ni hoga
+//console.log(div.style.textAlign="center");
+//div.innerText ="Hello!"; // but ye daalne se site pr change ho jayga
 
 // Insert Elements
+let newBtn = document.querySelector("button");
+newBtn.innerText ="click me!";
+console.log(newBtn);
+
+let p = document.querySelector("p");
+p.after(newBtn) // add at the end of node
+
+// append and prepend add inside the node
+
+// before add outside the node before node
+
+// after add the node outside after node
+
+let  h1= document.createElement("h1");
+h1.innerHTML = "<i>Hi, I am new!";
+
+document.querySelector("body").prepend(h1);
+
+// Remove
+let paragraph = document.querySelector("p");
+paragraph.remove();
+
+
+// Lets's Practice
+let but = document.createElement("button");
+document.querySelector("body").prepend(but);
+ but.style.backgroundColor="red";
+ but.style.color="white";
+ but.style.height="50px";
+ but.innerText = "click me";
+
+// Qs2
+let u = document.querySelector("p");
+console.log(u.getAttribute("class"));
+
+//u.setAttribute("class","newClass");
+
+// Class List mixes two classes properties
+u.classList.add("newClass");
